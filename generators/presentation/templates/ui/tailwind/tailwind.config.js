961fa3d426ad58faee552e273a2ae8fcfc260a66
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2021-08-24 19:23:33 -0300

    fix: fix tailwind config

--- generators/presentation/templates/ui/tailwind/tailwind.config.js
+++ generators/presentation/templates/ui/tailwind/tailwind.config.js
@@ -1,6 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './source/presentation/pages/**/*.{js,ts,jsx,tsx}',
+    './source/presentation/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},

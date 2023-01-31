c0853557ce12afba2a65d381b8812bf73d82e13c
João Vítor Carvalho <joaovitor.carvalho@novahaus.com.br>
2023-01-31 19:02:59 -0300

    feat: export new Error types

--- generators/core/templates/Errors/InternalServerError.ts
+++
@@ -1,9 +0,0 @@
-export class InternalServerError extends Error {
-  readonly error: Error;
-
-  constructor(err: Error) {
-    super(err.message);
-    this.error = err;
-    Object.setPrototypeOf(this, InternalServerError.prototype);
-  }
-}
